c0284e98d8d35e267b254578570713b7f570118e
radelmoral <rafael.delmoraljimenez@gmail.com>
2026-03-31 09:46:19 +0200

    db.js modificado

--- WEBPVP_V2/app/config/db.js
+++ WEBPVP_V2/app/config/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   port:     parseInt(process.env.DB_PORT) || 3306,
   database: process.env.DB_NAME     || 'save_pvp',
   user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS || process.env.DB_PASSWORD,
   waitForConnections: true,
   connectionLimit:    10,
   queueLimit:         0,
